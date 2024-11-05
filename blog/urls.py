61507efc2c4a47f89f09d9e1e5d9a074e6700c31
zoo <anfdnfl@khu.ac.kr>
2024-11-05 15:36:07 +0900

    photoblogserver

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("api_root/", include(router.urls)),
     path("post/<int:pk>/", views.post_detail, name='post_detail'),
     path("post/new/", views.post_new, name="post_new"),
-    path("post/<int:pk>/edit/", views.post_edit, name="post_edit")
+    path("post/<int:pk>/edit/", views.post_edit, name="post_edit"),
+    path("api_root/", include(router.urls)),
 ]
